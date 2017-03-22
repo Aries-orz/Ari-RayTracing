0937ec9a14034ce8b1d5153e3800a22df4e1ac07
Aries-orz <520007001@qq.com>
2017-03-22 16:09:23 +0800

    Update RTObject.h

--- src/RTObject.h
+++ src/RTObject.h
@@ -7,16 +7,16 @@ namespace rt {
 
 class RTObject {
 public:
-	enum Material { DIFFUSE, SPECULAR, REFRACTIVE };
-	Material mat;
-	Vec3d emission, color;
+    enum Material { DIFFUSE, SPECULAR, REFRACTIVE };
+    Material mat;
+    Vec3d emission, color;
 
     RTObject();
-	RTObject(Material m, Vec3d e, Vec3d c);
+    RTObject(Material m, Vec3d e, Vec3d c);
     virtual ~RTObject();
     
     //Compute whether the ray will hit the object, and return the distance if yes or return 0 if no
-	virtual double hit(const Ray& r) = 0;
+    virtual double hit(const Ray& r) = 0;
     //Calculate the normal if the ray hits
     virtual Vec3d getNormal(const Ray& r, const double t) const = 0;
 };
